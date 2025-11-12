bfc345f3753c416ee05a120016c3f54f3219adbd
FelipeKlinger <juanfelipetaborda9@gmail.com>
2025-11-12 16:42:08 +0100

    Modo desarrollo

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
+app.use(express.static('dist')); // middleware to serve static files from the 'dist' directory
 
 app.use(cors()); // middleware to enable CORS
-
 app.use(express.json()); // middleware to parse JSON bodies
 
 let notes = [
